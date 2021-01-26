8022d4240d5b11eaff7914896e3083f126a0d232
robertoshimizu <rshimizubr@gmail.com>
2021-01-26 19:25:06 -0300

    refactor: merge user repository to auth repository

--- lib/domain/repository/auth_repository.dart
+++ lib/domain/repository/auth_repository.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
 abstract class AuthRepository {
-  Future<void> sendOtp({@required String phoNo});
+  final HttpClient httpClient;
+  final String url;
+
+  // Login
+
+  AuthRepository({@required this.httpClient, @required this.url});
+
+  Future<bool> sendOtp({@required String phoNo});
 
   Future<void> verifyOtp({@required String verificationCode});
 
   Future<String> authenticateUser();
+
+  // Auth
+
+  Future<String> authenticate(
+      {@required String email, @required String password});
+
+  Future<void> deleteToken();
+
+  Future<void> persistToken({@required String token});
+
+  Future<bool> hasToken();
 }
+
+class HttpClient {}

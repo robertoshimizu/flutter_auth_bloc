6fdab4d94f573f99985690bf4d06b46f1d8c829e
robertoshimizu <rshimizubr@gmail.com>
2021-01-27 13:35:41 -0300

    chore: :heavy_plus_sign: setup firebase for IOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -26,7 +27,9 @@ class SimpleBlocObserver extends BlocObserver {
   }
 }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
   AuthRepository authRepository;
   // Needs to inject Auth Repository Implementation
